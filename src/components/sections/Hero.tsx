e9061cbd414b1af3a2ed2437b5fa48f3a7efc7d9
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-02-16 10:42:09 +0100

    Füge Hero- und LittleAboutMe-Komponenten hinzu, aktualisiere TypeScript-Pfade und implementiere Animationen

---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { footerItems } from "@/data";
+import Link from "next/link";
+import SplitText from "@/TextAnimations/SplitText";
+
+export default function Hero() {
+  const handleScrollToAbout = () => {
+    const aboutSection = document.getElementById("about-section");
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <section id="hero" className="px-7 pb-10 md:pb-40">
+      <div className="container mx-auto">
+        <IntroHeader />
+        <div className="max-w-8xl md:pl-10 mx-auto">
+          <div className="md:flex md:flex-row-reverse md:items-center md:justify-between md:gap-16 lg:gap-48">
+            <IntroParagraph />
+            <Portrait />
+          </div>
+
+          <div className="flex justify-between items-center">
+            <Location />
+            <ScrollForMore onClick={handleScrollToAbout} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+const IntroHeader = () => (
+  <h1 className="lg:translate-y-1/4 title md:text-lgTitle">
+    <SplitText text="HI THERE -" />
+    <SplitText text="I'M ALI RAMAZAN" className="justify-end" />
+  </h1>
+);
+
+const IntroParagraph = () => (
+  <motion.div
+    className="content my-8 text-gray md:text-lgContent lg:mr-auto md:pl-10 max-w-3xl"
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.8 }}
+  >
+    Experienced Software Developer. I love solving complex problems and building web solutions that
+    are fast, scalable, and future-ready.
+    <Socials />
+  </motion.div>
+);
+
+const Portrait = () => (
+  <Image
+    alt="Portrait of Ali Ramazan"
+    src="/me.jpeg"
+    width={408}
+    height={488}
+    className="rounded-md md:mx-0 mb-5 w-full lg:max-w-sm"
+    priority
+  />
+);
+
+const Socials = () => (
+  <div className="flex gap-4 justify-center items-center md:justify-start pt-5">
+    {footerItems.map((item) => (
+      <Link
+        key={item.path}
+        href={item.path}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label={`Go to ${item.title}`}
+        className="flex items-center gap-2 text-gray hover:text-black hover:scale-105 transition"
+      >
+        <Image
+          src={item.icon}
+          alt={`${item.title} Icon`}
+          width={32}
+          height={32}
+          priority={item.title === "LinkedIn"}
+        />
+        <span className="hidden md:inline">{item.title}</span>
+      </Link>
+    ))}
+  </div>
+);
+
+interface IconProps {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+  additionalClasses?: string;
+}
+
+const Icon: React.FC<IconProps> = ({ src, alt, width, height, additionalClasses = "" }) => (
+  <Image src={src} alt={alt} width={width} height={height} className={additionalClasses} />
+);
+
+const Location = () => (
+  <div className="button md:text-lgButton text-gray flex items-center gap-2">
+    <Icon
+      src="/icons/globe.svg"
+      alt="Globe icon"
+      width={16}
+      height={16}
+      additionalClasses="md:w-4 md:h-4 lg:w-6 lg:h-6"
+    />
+    <span>Frankfurt am Main</span>
+  </div>
+);
+
+const ScrollForMore = ({ onClick }: { onClick: () => void }) => (
+  <div className="button md:text-lgButton text-gray hidden md:flex md:items-center">
+    <button onClick={onClick} className="flex items-center gap-2">
+      <span>(Scroll For More)</span>
+      <motion.div
+        initial={{ y: 0 }}
+        animate={{ y: [0, 10, 0] }}
+        transition={{ duration: 1, repeat: Infinity }}
+      >
+        <Image
+          src="/icons/arrowdown.svg"
+          alt="Arrow Down Icon for scrolling"
+          width={16}
+          height={16}
+          className="md:w-4 md:h-4 lg:w-6 lg:h-6"
+        />
+      </motion.div>
+    </button>
+  </div>
+);
---
+++ src/components/sections/LittleAboutMe.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Marquee from "@/components/ui/Marquee";
+import SplitText from "@/TextAnimations/SplitText";
+
+const LittleAboutMe = () => {
+  return (
+    <section id="about-section" className="px-7 pt-12 md:pt-40 text-white">
+      <div className="container pb-12 mx-auto grid grid-cols-1 gap-4 md:grid-cols-2 md:pb-36 md:gap-0 items-start ">
+        <div>
+          <Header />
+          <Paragraph />
+        </div>
+        <div className="flex flex-col h-full md:pt-48">
+          <Paragraph2 />
+          <MoreAboutMe />
+        </div>
+      </div>
+      <Marquee />
+    </section>
+  );
+};
+
+const Header = () => (
+  <SplitText text="A Little About Me -" className="heading mb-5 md:text-lgHeading md:mb-16 " />
+);
+
+const fadeInVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
+
+const Paragraph = () => (
+  <motion.p
+    className="content md:pl-10 md:text-lgContent md:order-1"
+    variants={fadeInVariants}
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true, margin: "-100px" }}
+    transition={{ duration: 1, ease: "easeOut" }}
+  >
+    I am a Full-Stack Software Developer with extensive experience in modern web technologies such as JavaScript, Node.js, Next.js, React, Blazor, and .NET Core. Through my work on e-commerce platforms, microservices, and blog applications, I have developed a strong focus on scalable software architectures and best practices. My background includes hands-on development, problem-solving, and optimizing performance in web applications.
+  </motion.p>
+);
+
+const Paragraph2 = () => (
+  <motion.p
+    className="content md:text-lgContent md:order-1 max-w-2xl lg:ml-auto"
+    variants={fadeInVariants}
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true, margin: "-100px" }}
+    transition={{ duration: 2, ease: "easeOut" }}
+  >
+    JavaScript, C#, TypeScript, React, and Next.js—building real-world solutions through technology.
+    Thriving in collaborative environments, I am always looking for new ways to refine my skills and
+    create impactful solutions.
+  </motion.p>
+);
+
+interface IconProps {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+  additionalClasses?: string;
+}
+
+const Icon: React.FC<IconProps> = ({ src, alt, width, height, additionalClasses = "" }) => (
+  <Image src={src} alt={alt} width={width} height={height} className={additionalClasses} />
+);
+
+const MoreAboutMe = () => (
+  <motion.div
+    className="flex pt-5 items-center text-gray group md:order-0 max-w-lg md:pl-24 md:pb-10"
+    variants={fadeInVariants}
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true, margin: "-100px" }}
+    transition={{ duration: 2, ease: "easeOut" }}
+  >
+    <Link href="/about" passHref>
+      <span className="button text-gray mr-2 md:text-lgButton hover:text-white hover:underline transition">
+        (Know More About Me)
+      </span>
+    </Link>
+    <Link href="/about" passHref>
+      <Icon
+        src="/icons/arrowup.svg"
+        alt="Arrow Up Icon"
+        width={16}
+        height={16}
+        additionalClasses="transition-transform duration-300 group-hover:translate-x-1 group-hover:-translate-y-1 w-3 h-3 md:w-4 md:h-4 lg:w-5 lg:h-5"
+      />
+    </Link>
+  </motion.div>
+);
+
+export default LittleAboutMe;
