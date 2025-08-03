c568b1f9c92d241cdd48c2536529590f93da51e0
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-08-03 14:01:45 +0200

    style: replace img with Image component for logo in Nav for better optimization

--- components/ui/Nav.tsx
+++ components/ui/Nav.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { navItems } from "@/data";
 import { motion, AnimatePresence } from "framer-motion";
 import NoiseBackground from "../NoiseBackground";
+import Image from "next/image";
 
 interface NavItemType {
   title: string;
@@ -79,15 +80,22 @@ export const Nav = ({ className }: { className?: string }) => {
                 isProjectsPage ? "hover:text-white" : "hover:text-black"
               }`}
             >
-              <motion.img
-                src="/logo.svg"
-                alt="Logo"
-                className={`h-20 w-auto ${
-                  isProjectsPage ? "grayscale brightness-[0.9]" : "brightness-0"
-                } opacity-100`}
+              <motion.div
                 whileHover={{ scale: 1.1, rotate: 10 }}
                 transition={{ type: "spring", stiffness: 300 }}
-              />
+              >
+                <Image
+                  src="/logo.svg"
+                  alt="Logo"
+                  width={120}
+                  height={80}
+                  className={`h-20 w-auto ${
+                    isProjectsPage
+                      ? "grayscale brightness-[0.9]"
+                      : "brightness-0"
+                  } opacity-100`}
+                />
+              </motion.div>
             </Link>
 
             <div className="hidden lg:flex space-x-8">
