0a94aaea209ddeade33d6145565679bc7bd55b7b
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2025-08-13 12:11:31 +0200

    feat: Add creative title section to PartnersMarquee component

--- components/sections/PartnersMarquee.tsx
+++ components/sections/PartnersMarquee.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import MarqueeItem from "@/components/ui/MarqueeItem";
+import SplitText from "@/TextAnimations/SplitText";
 
 const PartnersMarquee: React.FC = () => {
   const partners = [
@@ -12,6 +13,19 @@ const PartnersMarquee: React.FC = () => {
 
   return (
     <div className="container mx-auto md:px-28 mt-16">
+      {/* Kreativer Titelbereich */}
+      <div className="text-center mb-8 md:mb-12">
+        <span className="uppercase tracking-[0.5em] font-semibold text-sm md:text-base lg:text-lg text-white/90">
+  Stronger together
+</span>
+
+        <div className="w-full flex justify-center">
+          <h2 className="title md:text-lgHeading font-extrabold mt-2 text-white w-fit">
+            <SplitText text="Our partners" />
+          </h2>
+        </div>
+      </div>
+
       <MarqueeItem images={partners} from={"0%"} to={"-100%"} />
     </div>
   );
