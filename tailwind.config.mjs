d95f3360cf0decc282709be867c99ae9d9718406
AliRamazanYildirim <94836230+AliRamazanYildirim@users.noreply.github.com>
2026-03-27 15:45:09 +0100

    refactor: migrate Tailwind CSS configuration from TypeScript to JavaScript and update related paths

---
+++ tailwind.config.mjs
@@ -0,0 +1,99 @@
+import plugin from "tailwindcss/plugin";
+import forms from "@tailwindcss/forms";
+import tailwindAnimate from "tailwindcss-animate";
+
+const config = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: "#131313",
+        white: "#eeede9",
+        gray: "#7D7D7D",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        "background-dark": "#0a0a0a",
+        "foreground-dark": "#ededed",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        satoshi: ["Satoshi", "sans-serif"],
+      },
+      fontSize: {
+        title: "36px",
+        heading: "30px",
+        content: "16px",
+        content2: "16px",
+        content3: "14px",
+        button: "14px",
+        lgTitle: "128px",
+        lgHeading: "80px",
+        lgContent: "30px",
+        lgContent2: "30px",
+        lgContent3: "24px",
+        lgButton: "24px",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        ".text-balance": {
+          textWrap: "balance",
+        },
+      });
+    }),
+    forms,
+    tailwindAnimate,
+  ],
+};
+
+export default config;
